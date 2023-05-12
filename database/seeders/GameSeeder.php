f13bdade5bcc77a9d3f2df95b68cfc0418c3940d
thuan122 <127835141+thuan122@users.noreply.github.com>
2023-05-12 15:14:01 +0700

    image optimized

--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -20,7 +20,7 @@ class GameSeeder extends Seeder
             'name' => 'Bloons TD 5',
             'description' => 'The Bloons are back and better than ever! Get ready for a massive 3D tower defense game designed to give you hours and hours of the best strategy gaming available.',
             'price' => 188000,
-            'image' => 'bt5.jpg',
+            'image' => 'bloons-td-5.webp',
             'publisher_id' => '8'
         ]);
 
@@ -28,7 +28,7 @@ class GameSeeder extends Seeder
             'name' => 'Bloons TD 6',
             'description' => 'Five-star tower defense with unrivaled depth and replayability. The Bloons are back in full HD glory and this time they mean business! Build awesome towers, choose your favorite upgrades, hire new Special Agents, and pop every last invading Bloon in the most popular tower defense series in history.',
             'price' => 120000,
-            'image' => 'bt6.jpg',
+            'image' => 'bloons-td-6.webp',
             'publisher_id' => '8'
         ]);
 
@@ -36,7 +36,7 @@ class GameSeeder extends Seeder
             'name' => 'Cyberpunk 2077',
             'description' => 'Cyberpunk 2077 is an open-world, action-adventure RPG set in the dark future of Night City — a dangerous megalopolis obsessed with power, glamor, and ceaseless body modification.',
             'price' => 990000,
-            'image' => 'cyberpunk.jpg',
+            'image' => 'cyberpunk-2077.webp',
             'publisher_id' => '4'
             
         ]);
@@ -45,7 +45,7 @@ class GameSeeder extends Seeder
             'name' => "Don't Starve Together",
             'description' => 'Don’t Starve is an uncompromising wilderness survival game full of science and magic. Enter a strange and unexplored world full of strange creatures, dangers, and surprises. Gather resources to craft items and structures that match your survival style.',
             'price' => 165000,
-            'image' => 'dst.jpg',
+            'image' => 'dont-starve-together.webp',
             'publisher_id' => '6'
             
         ]);
@@ -54,7 +54,7 @@ class GameSeeder extends Seeder
             'name' => 'ELDEN RING',
             'description' => 'THE NEW FANTASY ACTION RPG. Rise, Tarnished, and be guided by grace to brandish the power of the Elden Ring and become an Elden Lord in the Lands Between.',
             'price' => 800000,
-            'image' => 'er.jpg',
+            'image' => 'elden-ring.webp',
             'publisher_id' => '17'
             
         ]);
@@ -63,7 +63,7 @@ class GameSeeder extends Seeder
             'name' => 'Azure Striker Gunvolt',
             'description' => 'A 2D action game featuring a young man with the power of lightning who stands for freedom against the evil ambitions of a massive organization. When lightning strikes, a new legend is born.',
             'price' => 165000,
-            'image' => 'gv1.jpg',
+            'image' => 'azure-striker-gunvolt.webp',
             'publisher_id' => '15'
             
         ]);
@@ -72,7 +72,7 @@ class GameSeeder extends Seeder
             'name' => 'Azure Striker Gunvolt 2',
             'description' => 'The ultra-refined 2D side-scrolling action you remember from \"Azure Striker Gunvolt\" is recharged and better than ever! In Gunvolt 2, series anti-hero Copen joins the fray as a playable character! Copy abilities from defeated boss characters and use them as your own!',
             'price' => 188000,
-            'image' => 'gv2.jpg',
+            'image' => 'azure-striker-gunvolt-2.webp',
             'publisher_id' => '15'
             
         ]);
@@ -81,7 +81,7 @@ class GameSeeder extends Seeder
             'name' => 'Azure Striker Gunvolt 3',
             'description' => 'The next installment in the high-speed 2D action Gunvolt series is finally here with Azure Striker Gunvolt 3. Balancing both an involving story and satisfying gameplay, Gunvolt 3 is the most extravagant entry in the series yet!',
             'price' => 705000,
-            'image' => 'gv3.jpg',
+            'image' => 'azure-striker-gunvolt-3.webp',
             'publisher_id' => '15'
             
         ]);
@@ -90,7 +90,7 @@ class GameSeeder extends Seeder
             'name' => 'Hades',
             'description' => 'Defy the god of the dead as you hack and slash out of the Underworld in this rogue-like dungeon crawler from the creators of Bastion, Transistor, and Pyre.',
             'price' => 321000,
-            'image' => 'hades.jpg',
+            'image' => 'hades.webp',
             'publisher_id' => '11'
             
         ]);
@@ -99,7 +99,7 @@ class GameSeeder extends Seeder
             'name' => 'Left 4 Dead 2',
             'description' => 'Set in the zombie apocalypse, Left 4 Dead 2 (L4D2) is the highly anticipated sequel to the award-winning Left 4 Dead, the #1 co-op game of 2008. This co-operative action horror FPS takes you and your friends through the cities, swamps and cemeteries of the Deep South, from Savannah to New Orleans.',
             'price' => 142000,
-            'image' => 'l4d2.jpg',
+            'image' => 'left-4-dead-2.webp',
             'publisher_id' => '7'
         ]);
 
@@ -107,7 +107,7 @@ class GameSeeder extends Seeder
             'name' => 'Megaman 11',
             'description' => 'Mega Man is back! The newest entry in this iconic series blends classic, challenging 2D platforming action with a fresh new visual style. The new Double Gear system boosts Mega Man\'s speed and power for a new twist to the satisfying gameplay the series is known for.',
             'price' => 398000,
-            'image' => 'mm11.jpg',
+            'image' => 'megaman-11.webp',
             'publisher_id' => '12'
             
         ]);
@@ -116,7 +116,7 @@ class GameSeeder extends Seeder
             'name' => 'Persona 5 Royal',
             'description' => 'Don the mask and join the Phantom Thieves of Hearts as they stage grand heists, infiltrate the minds of the corrupt, and make them change their ways!',
             'price' => 1380000,
-            'image' => 'p5.jpg',
+            'image' => 'persona-5-royal.webp',
             'publisher_id' => '5'
         ]);
 
@@ -124,7 +124,7 @@ class GameSeeder extends Seeder
             'name' => 'Plants vs. Zombies GOTY Edition',
             'description' => 'Zombies are invading your home, and the only defense is your arsenal of plants! Armed with an alien nursery-worth of zombie-zapping plants like peashooters and cherry bombs, you\'ll need to think fast and plant faster to stop dozens of types of zombies dead in their tracks.',
             'price' => 70000,
-            'image' => 'pvz.jpg',
+            'image' => 'plants-vs-zombies-goty-edition.webp',
             'publisher_id' => '2'
         ]);
 
@@ -132,7 +132,7 @@ class GameSeeder extends Seeder
             'name' => 'Starbound',
             'description' => 'The Bloons are back and better than ever! Get ready for a massive 3D tower defense game designed to give you hours and hours of the best strategy gaming available.',
             'price' => 188000,
-            'image' => 'starbound.jpg',
+            'image' => 'starbound.webp',
             'publisher_id' => '3'
         ]);
 
@@ -140,7 +140,7 @@ class GameSeeder extends Seeder
             'name' => 'Stardew Valley',
             'description' => "You've inherited your grandfather's old farm plot in Stardew Valley. Armed with hand-me-down tools and a few coins, you set out to begin your new life. Can you learn to live off the land and turn these overgrown fields into a thriving home?",
             'price' => 165000,
-            'image' => 'stardew.jpg',
+            'image' => 'stardew-valley.webp',
             'publisher_id' => '14'
         ]);
 
@@ -148,7 +148,7 @@ class GameSeeder extends Seeder
             'name' => 'Stray',
             'description' => 'Lost, alone and separated from family, a stray cat must untangle an ancient mystery to escape a long-forgotten cybercity and find their way home.',
             'price' => 319000,
-            'image' => 'stray.jpg',
+            'image' => 'stray.webp',
             'publisher_id' => '9'
         ]);
 
@@ -156,7 +156,7 @@ class GameSeeder extends Seeder
             'name' => 'Terraria',
             'description' => 'Dig, fight, explore, build! Nothing is impossible in this action-packed adventure game. Four Pack also available!',
             'price' => 120000,
-            'image' => 'terraria.jpg',
+            'image' => 'terraria.webp',
             'publisher_id' => '1'
         ]);
 
@@ -164,7 +164,7 @@ class GameSeeder extends Seeder
             'name' => 'Undertale',
             'description' => "UNDERTALE! The RPG game where you don't have to destroy anyone.",
             'price' => 120000,
-            'image' => 'undertale.jpg',
+            'image' => 'undertale.webp',
             'publisher_id' => '13'
         ]);
 
@@ -172,7 +172,7 @@ class GameSeeder extends Seeder
             'name' => 'Valiant Hearts: The Great War™',
             'description' => 'Valiant Hearts : The Great War is the story of 4 crossed destinies and a broken love in a world torn apart. Dive into a 2D animated comic book adventure, mixing exploration, action and puzzles. Lost in the middle of the trenches, play as each of the 4 strangers, relive the War and help a young German',
             'price' => 249000,
-            'image' => 'valiant.jpg',
+            'image' => 'valiant-hearts-the-great-war.webp',
             'publisher_id' => '10'
         ]);
 
@@ -180,7 +180,7 @@ class GameSeeder extends Seeder
             'name' => 'Mega Man X Legacy Collection',
             'description' => 'Mega Man X Legacy Collection includes the legendary 16-bit titles and the series’ exciting foray into the 32-bit era: Mega Man X, Mega Man X2, Mega Man X3, and Mega Man X4. Test your skills in the new X Challenge mode, which pits players against two deadly bosses at once.',
             'price' => 276000,
-            'image' => 'xlc1.jpg',
+            'image' => 'megaman-x-legacy-collection.webp',
             'publisher_id' => '12'
         ]);
 
@@ -188,7 +188,7 @@ class GameSeeder extends Seeder
             'name' => 'Mega Man X Legacy Collection 2',
             'description' => 'Complete the exciting Mega Man X saga with four action-packed titles! This collection showcases the evolution of the series with Mega Man X5, Mega Man X6, Mega Man X7, and Mega Man X8. Test your skills in the new X Challenge mode, which pits players against two deadly bosses at once.',
             'price' => 276000,
-            'image' => 'xlc2.jpg',
+            'image' => 'megaman-x-legacy-collection-2.webp',
             'publisher_id' => '12'
         ]);
 
@@ -196,7 +196,7 @@ class GameSeeder extends Seeder
             'name' => 'Mega Man Zero/ZX Legacy Collection',
             'description' => 'Mega Man Zero/ZX Legacy Collection brings together six classic titles in one game: Mega Man Zero 1, 2, 3 and 4, as well as Mega Man ZX and ZX Advent. The collection also features Z-Chaser, an exclusive new mode created just for this set of games.',
             'price' => 550000,
-            'image' => 'z-zxlc.jpg',
+            'image' => 'megaman-zero-zx-legacy-collection.webp',
             'publisher_id' => '12'
         ]);
 
@@ -204,7 +204,7 @@ class GameSeeder extends Seeder
             'name' => 'Mega Man Legacy Collection',
             'description' => 'Mega Man Legacy Collection is a celebration of the 8-bit history of Capcom’s iconic Blue Bomber featuring faithful reproductions of the series’ origins with the original six Mega Man games.',
             'price' => 206000,
-            'image' => 'mlc1.jpg',
+            'image' => 'megaman-legacy-collection.webp',
             'publisher_id' => '12'
         ]);
 
@@ -212,7 +212,7 @@ class GameSeeder extends Seeder
             'name' => 'Mega Man Legacy Collection 2',
             'description' => 'The Blue Bomber is back in this second collection of classic Capcom titles! Featuring faithful reproductions of Mega Man 7, 8, 9, & 10, this collection is bursting with additional content, from time trials and remix challenges to a music player and an extensive gallery of rare illustrations.',
             'price' => 276000,
-            'image' => 'mlc2.jpg',
+            'image' => 'megaman-legacy-collection-2.webp',
             'publisher_id' => '12'
         ]);
     }
